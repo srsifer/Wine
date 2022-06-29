f9a16a41c53a9f2f62f1ed2b0a4822da55db0d19
igor s fernandes <igorsife80@gmail.com>
2022-06-29 19:29:51 -0300

    init: commit inicial :tada:

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import  Logo from '../assets/black.svg'
+
+const Home: React.FC = () => {
+  return (
+    <>
+      <h1>commit inicial</h1>
+      <Logo />
+    </>
+    )
+  }
+
+  export default Home

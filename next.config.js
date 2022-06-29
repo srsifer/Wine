56aac8eb11bb69d80db213e17b5cec197baf5ffb
igor s fernandes <igorsife80@gmail.com>
2022-06-29 19:25:28 -0300

    chore: configuraçoes do next-images e inLine-svg  :wrench:

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
-/** @type {import('next').NextConfig} */
+const withImages = require('next-images')
+
 const nextConfig = {
-  reactStrictMode: true,
+  withImages: withImages({ esModule: true}),
+  rectStrictMode: true
 }
 
 module.exports = nextConfig

302889763d441a39c12efc2d525d1e922e44886f
igor s fernandes <igorsife80@gmail.com>
2022-06-30 17:19:57 -0300

    :sparkles: feat: realiza requisição com swr

---
+++ src/utils/typesItem.ts
@@ -0,0 +1,26 @@
+export interface Item {
+  items: [
+    {
+      avaliations: number;
+      classification: string;
+      country: string;
+      discount: number;
+      flag: string;
+      id: number;
+      image: string;
+      name: string;
+      price: number;
+      priceMember: number;
+      priceNonMember: number;
+      rating: number;
+      region: string;
+      size: string;
+      sommelierComment: string;
+      type: string;
+    }
+  ],
+  itemsPerPage: number;
+  page: number;
+  totalItems: number;
+  totalPages: number;
+}

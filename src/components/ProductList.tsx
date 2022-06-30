5c09021cf63052964947040c8147d4476f68cecc
igor s fernandes <igorsife80@gmail.com>
2022-06-30 14:27:41 -0300

    :lipstick: feat adiciona estrutura e estilização de product list

---
+++ src/components/ProductList.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { ProductListStylesDiv } from '../styles/components/ProductListStyles'
+
+const ProductList: React.FC = () => {
+  return (
+    <ProductListStylesDiv>ProductList</ProductListStylesDiv>
+  )
+}
+
+export default ProductList
---
+++ src/styles/components/ProductListStyles.tsx
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const ProductListStylesDiv = styled.div`
+  min-width: fit-content;
+  max-width: 100%;
+  border: 1px solid red;
+`;

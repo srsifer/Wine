f5a4af2e0e20cda92baaf7070b4afb8047ff3a06
igor s fernandes <igorsife80@gmail.com>
2022-06-30 17:22:30 -0300

    :sparkles: feat: renderiza a lista de produtos com componentes  estilizados

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,9 +1,32 @@
 import React from 'react'
-import { ProductListStylesDiv } from '../styles/components/ProductListStyles'
+import { DivProductsFound, MainContentDivList, ProductListStylesDiv } from '../styles/components/ProductListStyles'
+import { useFetch } from '../utils/getAPIWithSwr'
+import { DataProps } from '../utils/typesItem'
+import ProductCard from './ProductCard'
 
 const ProductList: React.FC = () => {
+  const {data} = useFetch<DataProps>('https://wine-back-test.herokuapp.com/products?page=1&limit=10')
+  console.log(data)
+
+  if(!data) {
+    return <h1>carregando ...</h1>
+  }
+
   return (
-    <ProductListStylesDiv>ProductList</ProductListStylesDiv>
+    <MainContentDivList>
+    <DivProductsFound>
+      <h3>{`${data.totalItems} produtos foram encontrados`}</h3>
+    </DivProductsFound>
+    <ProductListStylesDiv>
+      {
+        data.items.map((item) => {
+          return(
+            <ProductCard item={item}/>
+          )
+        })
+      }
+    </ProductListStylesDiv>
+    </MainContentDivList>
   )
 }
 
--- src/styles/components/ProductListStyles.tsx
+++ src/styles/components/ProductListStyles.tsx
@@ -1,7 +1,24 @@
 import styled from "styled-components";
 
 export const ProductListStylesDiv = styled.div`
-  min-width: fit-content;
-  max-width: 100%;
-  border: 1px solid red;
+  max-width: 80%;
+  margin-left: 4%;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  flex-wrap: wrap;
+  align-items: center;
 `;
+
+export const DivProductsFound = styled.div`
+margin-left: 4%;
+padding: 3%;
+`
+export const MainContentDivList = styled.div `
+  max-width: 80%;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+  text-align: center
+`

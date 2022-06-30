7894632a8956c4423fe88b691c1dc41f6419c8da
igor s fernandes <igorsife80@gmail.com>
2022-06-30 17:54:04 -0300

    :bug: fix: corrige o nome  das labels de filter by price

--- src/components/FilterByPrice.tsx
+++ src/components/FilterByPrice.tsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import React, { JSXElementConstructor } from 'react'
 import { FilterPriceUlStyles, FilterStylesDiv } from '../styles/components/FilterByPriceStyles'
 
-const FilterByPrice: React.FC = () => {
+const FilterByPrice: React.FC = (): JSX.Element  => {
   return (
     <FilterStylesDiv>
       <div>
@@ -9,31 +9,31 @@ const FilterByPrice: React.FC = () => {
         <h4> por preço</h4>
       </div>
       <FilterPriceUlStyles>
-      <label name='filter-price' >
+      <label >
         <li>
           <input type="radio" name='filter-price'/>
           <p>Até R$ 40</p>
         </li>
         </label>
-      <label name="filter-price" >
+      <label  >
         <li>
           <input type="radio" name='filter-price'/>
           <p>R$ 40 A R$ 60</p>
         </li>
       </label>
-      <label name="filter-price" >
+      <label  >
         <li>
           <input type="radio" name='filter-price'/>
           <p>R$ 100 A R$ 200</p>
         </li>
       </label>
-      <label name="filter-price" >
+      <label  >
         <li>
           <input type="radio" name='filter-price'/>
           <p>R$ 200 A R$ 500</p>
         </li>
       </label>
-      <label name='filter-price' >
+      <label >
         <li>
           <input type="radio" name='filter-price'/>
           <p>A cima de R$ 500</p>

5a116f31c27d8e5a01c88200ab7276598ee0275d
igor s fernandes <igorsife80@gmail.com>
2022-06-30 14:28:14 -0300

    :lipstick: feat invoca filter by price e productlist no index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import  Logo from '../assets/black.svg'
+import  FilterByPrice  from '../components/FilterByPrice'
 import NavBarHeader from '../components/NavBarHeader'
+import ProductList from '../components/ProductList'
+import { StylesDiv } from '../styles/pages/indexStyles'
 
 const Home: React.FC = () => {
   return (
     <>
-    <NavBarHeader />
+      <NavBarHeader />
+      <StylesDiv>
+        <FilterByPrice />
+        <ProductList />
+      </StylesDiv>
     </>
     )
   }
---
+++ src/styles/pages/indexStyles.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+export const StylesDiv = styled.main`
+  display: flex;
+  justify-content: space-evenly;
+  border: 2px solid brown;
+  min-width: fit-content;
+  width: 80%;
+  margin-left: auto;
+  margin-right: auto;
+  min-height: fit-content;
+  max-height: 100vh;
+  height: 90%;
+  margin-top: 40px;
+`;

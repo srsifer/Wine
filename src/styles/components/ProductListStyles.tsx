414e2bd4943ac522f37b365d8765c0584f428b5b
igor s fernandes <igorsife80@gmail.com>
2022-07-01 00:35:18 -0300

    ok_hand :ok_hand: style: corrige largura pra tamanho relativo

--- src/styles/components/ProductListStyles.tsx
+++ src/styles/components/ProductListStyles.tsx
@@ -1,8 +1,9 @@
 import styled from "styled-components";
 
 export const ProductListStylesDiv = styled.div`
-  max-width: 80%;
+  max-width: 70vw;
   margin-left: 4%;
+  min-width: 70vw;
   display: flex;
   flex-direction: row;
   justify-content: center;
@@ -11,14 +12,13 @@ export const ProductListStylesDiv = styled.div`
 `;
 
 export const DivProductsFound = styled.div`
-margin-left: 4%;
-padding: 3%;
+  margin-left: 4%;
+  padding: 3%;
 `
-export const MainContentDivList = styled.div `
-  max-width: 80%;
+export const MainContentDivList = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: flex-start;
   align-items: flex-start;
-  text-align: center
+  text-align: center;
 `

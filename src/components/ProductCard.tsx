856660d750d02bcccf74ff7dabbea051960ea30f
igor s fernandes <igorsife80@gmail.com>
2022-07-01 20:04:54 -0300

    :sparkles: feat: adiciona redirecionamento pra tela de tealhes de produto ao clicar na imagem

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,45 +1,51 @@
+import Link from 'next/link';
 import React from 'react'
 import BlackFriday from '../assets/black.svg'
 import { DisplayCardInfo, DivPriceAndDiscount, DivPriceMember, DivPriceNoMember, ProductCardDisplay } from '../styles/components/ProductCardStyles';
 interface ProductCardProps {
   item: {
-      id: number;
-      image: string;
-      name: string;
-      price: number;
-      priceMember: number;
-      priceNonMember: number;
-      discount: number;
+    id: number;
+    image: string;
+    name: string;
+    price: number;
+    priceMember: number;
+    priceNonMember: number;
+    discount: number;
   }
 }
 
-export default function ProductCard({item}: ProductCardProps) {
+export default function ProductCard({ item }: ProductCardProps) {
   return (
     <ProductCardDisplay>
       <DisplayCardInfo key={item.id}>
-      <div>
-        <img src={item.image} alt={`imagem do vinho ${item.name}`} />
-        <BlackFriday  />
-      </div>
-      <h3>{item.name}</h3>
-      <DivPriceAndDiscount>
-        <p>{`R$ ${item.price}`}</p>
         <div>
-          {`${item.discount} % OFF`}
+          <Link href={`./${item.id}`}>
+            <a>
+              <img src={item.image} alt={`imagem do vinho ${item.name}`} />
+              <BlackFriday />
+            </a>
+          </Link>
         </div>
-      </DivPriceAndDiscount>
-      <DivPriceMember>
-        <p>SÓCIO WINE </p>
-        <div>{`R$${item.priceMember}`}</div>
-      </DivPriceMember>
-      <DivPriceNoMember>
-        <p>NÃO SÓCIO</p>
-        <div>{`R$${item.priceNonMember}`}</div>
-      </DivPriceNoMember>
-    </DisplayCardInfo>
-    <button>
-      Adicionar
-    </button>
+        <h3>{item.name}</h3>
+        <DivPriceAndDiscount>
+          <p>{`R$ ${item.price}`}</p>
+          <div>
+            {`${item.discount} % OFF`}
+          </div>
+        </DivPriceAndDiscount>
+        <DivPriceMember>
+          <p>SÓCIO WINE </p>
+          <div>{`R$${item.priceMember}`}</div>
+        </DivPriceMember>
+        <DivPriceNoMember>
+          <p>NÃO SÓCIO</p>
+          <div>{`R$${item.priceNonMember}`}</div>
+        </DivPriceNoMember>
+
+      </DisplayCardInfo>
+      <button>
+        Adicionar
+      </button>
     </ProductCardDisplay>
   )
 }

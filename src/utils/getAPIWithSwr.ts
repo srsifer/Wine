a612a0c85553aaeaac19b58e12b597a499be3395
igor s fernandes <igorsife80@gmail.com>
2022-07-01 00:38:11 -0300

    :recycle: refactor: tranforma os hulks de requisição do swr pra um unico hook de requisição

--- src/utils/getAPIWithSwr.ts
+++ src/utils/getAPIWithSwr.ts
@@ -1,7 +1,15 @@
 import useSWR from "swr";
-
-export function useFetch<Data = any>(url: string) {
+export interface UseFetchProps {
+  priceStart?:  number ,
+  priceEnd?: number ,
+  ProductName?: string ,
+  page?: string ,
+}
+export function useFetch<Data = any>({priceStart, priceEnd, ProductName, page}: UseFetchProps) {
+  const url = `
+  https://wine-back-test.herokuapp.com/products?filter=${!priceStart ? 0 : priceStart }-${!priceEnd  ? 600 : priceEnd}&page=${!page ? 1 : page}&name=${!ProductName ? '': ProductName }&limit=9`
   const {data, error} = useSWR<Data>(url, async url => {
+    console.log(url)
     const response = await fetch(url);
     const data = await response.json();
     return data

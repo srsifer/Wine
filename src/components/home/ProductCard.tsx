9b8af18539ccf8064627656d7f98983948a3eeba
igor s fernandes <igorsife80@gmail.com>
2022-07-02 15:03:13 -0300

    :sparkles: feat: adiciona função pra salvar item no local estorage

--- src/components/home/ProductCard.tsx
+++ src/components/home/ProductCard.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import React from 'react'
+import React, { useState } from 'react'
 import BlackFriday from '../../assets/black.svg'
 import {
   DisplayCardInfo,
@@ -22,6 +22,50 @@ interface ProductCardProps {
 }
 
 export default function ProductCard({ item }: ProductCardProps) {
+
+  const [dataOrder, setDataOrder] = useState({})
+
+  const createObjectOrder = () => {
+    const { name, priceMember, id } = item;
+    const objectOrder = {
+      id,
+      name,
+      priceMember,
+      quantity: 1
+    }
+    return objectOrder;
+  }
+
+  const AddToCart = () => {
+    const itemOrder = createObjectOrder()
+    const AllProducts = JSON.parse(localStorage.getItem('allProducts') || '[]')
+    console.log(typeof AllProducts);
+
+    if (AllProducts.length == 0) {
+      console.log("entrei no if")
+     return localStorage.setItem('allProducts', JSON.stringify([itemOrder]))
+    }
+    console.log('não terminei')
+    const itemFound = AllProducts.find((item: typeof itemOrder) => item.id === itemOrder.id);
+
+    if (itemFound) {
+      AllProducts.forEach((localitem: typeof itemOrder) => {
+        if (localitem.id === itemOrder.id) {
+          localitem.priceMember += itemOrder.priceMember
+          localitem.quantity += 1
+        }
+      })
+    }
+
+    if (itemFound == undefined) {
+      console.log("entrei na terceira condição")
+      AllProducts.push(itemOrder)
+      console.log(AllProducts)
+    }
+    localStorage.setItem('allProducts', JSON.stringify(AllProducts))
+    console.log(itemFound)
+  }
+
   return (
     <ProductCardDisplay>
       <DisplayCardInfo key={item.id}>
@@ -50,7 +94,9 @@ export default function ProductCard({ item }: ProductCardProps) {
         </DivPriceNoMember>
 
       </DisplayCardInfo>
-      <button>
+      <button
+        onClick={AddToCart}
+      >
         Adicionar
       </button>
     </ProductCardDisplay>
